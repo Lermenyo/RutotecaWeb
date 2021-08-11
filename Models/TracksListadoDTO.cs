db6ea50fcc040e482f7f673a5bd0dc55f0e22d90
B. Abel Perez <abel.perez@entelgy.com>
2021-08-11 20:09:32 +0200

    BUG: error con Time en GPX solucionado

--- Models/TracksListadoDTO.cs
+++ Models/TracksListadoDTO.cs
@@ -16,7 +16,7 @@ namespace RutotecaWeb.Models
         public Decimal Ascenso      { get; set; }
         public Decimal Descenso     { get; set; }
         public Decimal KmSubida     { get; set; }
-        public DateTime Tiempo      { get; set; }
+        public TimeSpan Tiempo      { get; set; }
         public String Color         { get; set; }
 
         public static string SELECT_COMPLETA =
